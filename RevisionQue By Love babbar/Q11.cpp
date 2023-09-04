dc6957c05786543cac2f5cea095e6476f3630a52
Jayesh Kumar Verma <111409049+jayeshverma001@users.noreply.github.com>
2023-09-04 23:25:10 +0530

    Add files via upload

---
+++ RevisionQue By Love babbar/Q11.cpp
@@ -0,0 +1,24 @@
+// Pattern Problem : 
+// ***
+// ***
+// ***
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<"*"<<endl;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
