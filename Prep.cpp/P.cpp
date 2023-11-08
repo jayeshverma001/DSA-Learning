f0fc7b5d42941c69d9ada5d709331496dd983872
Jayesh Kumar Verma <111409049+jayeshverma001@users.noreply.github.com>
2023-11-08 23:21:42 +0530

    Add files via upload

---
+++ Prep.cpp/P.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+       int j =1;
+       cout<<j;
+       j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
